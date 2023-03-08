5d08110c3de9123edc0500bc940830a57fae7c7f
MFarooq07 <93149539+MFarooq07@users.noreply.github.com>
2023-03-08 10:56:24 -0500

    Create entryway_light.cpp

---
+++ modules/entryway_light/entryway_light.cpp
@@ -0,0 +1,27 @@
+
+#include ldr_sensor.h
+#include "mbed.h"
+#include "arm_book_lib.h"
+#include "ldr_sensor.h"
+#include "smart_home_system.h"
+
+
+DigitalOut eLED(PE_10);
+
+
+void entrywayLEDon()
+{
+  while (true)
+  {
+    eLED = ON;
+  }
+}
+
+
+void entrywayLEDoff()
+{
+  while (true)
+  {
+    eLED = OFF;
+  }
+}
