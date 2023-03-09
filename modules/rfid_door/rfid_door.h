518162576ee49117d379bc7c40d61eb87104552d
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 21:12:55 -0500

    Create rfid_door.h

---
+++ modules/rfid_door/rfid_door.h
@@ -0,0 +1,17 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _RFID_DOOR_H_
+#define _RFID_DOOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void doorInit();
+void doorUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _RFID_DOOR_H_
