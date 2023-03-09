4b275025532766f2a61eead9b5e10d5ca983c477
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-09 11:17:43 -0500

    Update servo_motor.cpp

--- modules/servo_motor/servo_motor.cpp
+++ modules/servo_motor/servo_motor.cpp
@@ -4,9 +4,10 @@
 #include "arm_book_lib.h"
 #include "servo_motor.h"
 // #include "ldr_sensor.h"
+#include "rgb_led.h"
 
 #define DUTY_MIN 0.025
-#define DUTY_MAX 0.07   
+#define DUTY_MAX 0.09 
 #define PERIOD 0.02
 
 PwmOut servo(PF_9);
@@ -17,6 +18,7 @@ DigitalOut testLED(LED2);   //can be used in the code below to test if code runs
 //servo_doorInit: changed from the servo_gateInit. Closes the door (move to original position)
 void servo_doorInit()        
 {
+    rgbLedInit();
     servo.period(PERIOD);
     servo.write(DUTY_MIN);
 }
@@ -24,6 +26,7 @@ void servo_doorInit()
 //opens door for 10 seconds; the function that calls openDoor should also call servo_doorInit to close the door 
 void openDoor() //changed from raiseGate
 {
+    //toGreenRgbLed();
     servo.write(DUTY_MAX);  
     delay(10000);
 }
