e06055425bf7b9082debf1b85358ea6ca09c399c
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 21:16:21 -0500

    Create rgb_led.h

---
+++ modules/rgb_led/rgb_led.h
@@ -0,0 +1,20 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _RGB_LED_H_
+#define _RGB_LED_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+//void rgbInit();
+void toGreenRgbLed();
+void toGreenRgbLed();
+void rgbLedInit();
+
+
+//=====[#include guards - end]=================================================
+
+#endif // _RGB_LED_H_
