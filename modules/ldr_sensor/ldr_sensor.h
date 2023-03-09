6dab1754bcfb2db95d7686f1cd5dbf8eb94bdc8d
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 21:08:41 -0500

    Update ldr_sensor.h

--- modules/ldr_sensor/ldr_sensor.h
+++ modules/ldr_sensor/ldr_sensor.h
@@ -1,1 +1,21 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _LDR_SENSOR_H_
+#define _LDR_SENSOR_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void ldrSensorInit();
+void ldrSensorUpdate();
+float ldrSensorRead();
+float gldrSensorRead();
+float rldrSensorRead();
+
+//=====[#include guards - end]=================================================
+
+#endif // _SIREN_H_
 
