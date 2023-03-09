cc69df026dd4022abf3a8d9599533a632742b202
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 21:05:49 -0500

    Update entryway.h

--- modules/entryway/entryway.h
+++ modules/entryway/entryway.h
@@ -1,1 +1,17 @@
+//=====[#include guards - begin]===============================================
 
+#ifndef _ENTRYWAY_H_
+#define _ENTRYWAY_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void entrywayLightInit();
+void entrywayLightUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _ENTRYWAY_LIGHT_H_
