ad6645187d0849e9f889dd034d3cdd163cebee47
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 21:17:54 -0500

    Create servo_motor.h

---
+++ modules/servo_motor/servo_motor.h
@@ -0,0 +1,9 @@
+//=====[#include guards - begin]===============================================
+
+#ifndef _SERVO_DOOR_H_
+#define _SERVO_DOOR_H_
+
+void servo_doorInit();
+void openDoor();
+
+#endif
