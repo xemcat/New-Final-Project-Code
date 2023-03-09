0bbeb14e893d2237d23df7cd3072b0d83c002250
xemcat <93665141+xemcat@users.noreply.github.com>
2023-03-08 21:05:09 -0500

    Update entryway.cpp

--- modules/entryway/entryway.cpp
+++ modules/entryway/entryway.cpp
@@ -1,1 +1,46 @@
+//=====[Libraries]=============================================================
 
+#include "mbed.h"
+#include "arm_book_lib.h"
+
+#include "entryway.h"
+#include "ldr_sensor.h"
+
+#include "smart_home_system.h"
+
+//=====[Declaration of private defines]========================================
+
+//=====[Declaration of private data types]=====================================
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut entrywayLight(PE_10);
+
+//=====[Declaration of external public global variables]=======================
+
+//=====[Declaration and initialization of public global variables]=============
+
+//=====[Declaration and initialization of private global variables]============
+
+
+
+//=====[Declarations (prototypes) of private functions]========================
+
+//=====[Implementations of public functions]===================================
+
+
+
+//=====[Implementations of private functions]==================================
+
+void entrywayLightInit(){
+    entrywayLight = OFF;
+}
+
+void entrywayLightUpdate(){
+    ldrSensorUpdate();
+    if(ldrSensorRead()==true){
+        entrywayLight =OFF;
+    }else{
+        entrywayLight=ON;
+    }
+}
